f30d6a75aedf8389103c1ccea74ae1353cbd6d01
JuradoJr <104862099+JuradoJr@users.noreply.github.com>
2022-09-23 11:21:02 -0700

    Add files via upload

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,7 @@
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data = MechaCar_Table)
+
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data = MechaCar_Table))
+
+
