200ee4ef31823d6e246d08457a54d358cac457c7
JuradoJr <104862099+JuradoJr@users.noreply.github.com>
2022-09-23 11:41:50 -0700

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,3 +1,5 @@
+MechaCar_Table <- read.csv(file='Desktop/MechaCar_Statistical_Analysis/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
 lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
      ground_clearance + AWD, data = MechaCar_Table)
 
